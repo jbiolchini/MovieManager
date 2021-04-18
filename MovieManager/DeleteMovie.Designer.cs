fe8201b89483617ab8e3a71e4226593f479ace1a
George Raedeke <graedeke@student.cscc.edu>
2021-04-17 21:07:34 -0400

    Delete and Update Forms

--- MovieManager/DeleteMovie.Designer.cs
+++ MovieManager/DeleteMovie.Designer.cs
@@ -29,19 +29,229 @@ namespace MovieManager
         /// </summary>
         private void InitializeComponent()
         {
+            this.labelDeleteMovieScreen = new System.Windows.Forms.Label();
+            this.labelMovieTitleDelete = new System.Windows.Forms.Label();
+            this.tbMovieTitleDelete = new System.Windows.Forms.TextBox();
+            this.btFindDelete = new System.Windows.Forms.Button();
+            this.labelYearDelete = new System.Windows.Forms.Label();
+            this.tbYearDelete = new System.Windows.Forms.TextBox();
+            this.labelDirectorDelete = new System.Windows.Forms.Label();
+            this.tbDirectorDelete = new System.Windows.Forms.TextBox();
+            this.labelGenreDelete = new System.Windows.Forms.Label();
+            this.comboBoxGenreDelete = new System.Windows.Forms.ComboBox();
+            this.labelRottenTomatoesScoreDelete = new System.Windows.Forms.Label();
+            this.tbRottenTomatoesScoreDelete = new System.Windows.Forms.TextBox();
+            this.labelBoxOfficeEarningsDelete = new System.Windows.Forms.Label();
+            this.tbBoxOfficeEarningsDelete = new System.Windows.Forms.TextBox();
+            this.panel1Delete = new System.Windows.Forms.Panel();
+            this.btDeleteDelete = new System.Windows.Forms.Button();
+            this.btClearDelete = new System.Windows.Forms.Button();
+            this.btCloseDelete = new System.Windows.Forms.Button();
+            this.panel1Delete.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // labelDeleteMovieScreen
+            // 
+            this.labelDeleteMovieScreen.AutoSize = true;
+            this.labelDeleteMovieScreen.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDeleteMovieScreen.Location = new System.Drawing.Point(12, 9);
+            this.labelDeleteMovieScreen.Name = "labelDeleteMovieScreen";
+            this.labelDeleteMovieScreen.Size = new System.Drawing.Size(257, 29);
+            this.labelDeleteMovieScreen.TabIndex = 0;
+            this.labelDeleteMovieScreen.Text = "Delete Movie Screen";
+            // 
+            // labelMovieTitleDelete
+            // 
+            this.labelMovieTitleDelete.AutoSize = true;
+            this.labelMovieTitleDelete.Location = new System.Drawing.Point(52, 63);
+            this.labelMovieTitleDelete.Name = "labelMovieTitleDelete";
+            this.labelMovieTitleDelete.Size = new System.Drawing.Size(62, 13);
+            this.labelMovieTitleDelete.TabIndex = 1;
+            this.labelMovieTitleDelete.Text = "Movie Title:";
+            // 
+            // tbMovieTitleDelete
+            // 
+            this.tbMovieTitleDelete.Location = new System.Drawing.Point(117, 60);
+            this.tbMovieTitleDelete.Name = "tbMovieTitleDelete";
+            this.tbMovieTitleDelete.Size = new System.Drawing.Size(297, 20);
+            this.tbMovieTitleDelete.TabIndex = 2;
+            // 
+            // btFindDelete
+            // 
+            this.btFindDelete.Location = new System.Drawing.Point(420, 58);
+            this.btFindDelete.Name = "btFindDelete";
+            this.btFindDelete.Size = new System.Drawing.Size(75, 23);
+            this.btFindDelete.TabIndex = 3;
+            this.btFindDelete.Text = "Find";
+            this.btFindDelete.UseVisualStyleBackColor = true;
+            // 
+            // labelYearDelete
+            // 
+            this.labelYearDelete.AutoSize = true;
+            this.labelYearDelete.Location = new System.Drawing.Point(79, 103);
+            this.labelYearDelete.Name = "labelYearDelete";
+            this.labelYearDelete.Size = new System.Drawing.Size(32, 13);
+            this.labelYearDelete.TabIndex = 4;
+            this.labelYearDelete.Text = "Year:";
+            // 
+            // tbYearDelete
+            // 
+            this.tbYearDelete.Location = new System.Drawing.Point(117, 100);
+            this.tbYearDelete.Name = "tbYearDelete";
+            this.tbYearDelete.Size = new System.Drawing.Size(116, 20);
+            this.tbYearDelete.TabIndex = 5;
+            // 
+            // labelDirectorDelete
+            // 
+            this.labelDirectorDelete.AutoSize = true;
+            this.labelDirectorDelete.Location = new System.Drawing.Point(64, 143);
+            this.labelDirectorDelete.Name = "labelDirectorDelete";
+            this.labelDirectorDelete.Size = new System.Drawing.Size(47, 13);
+            this.labelDirectorDelete.TabIndex = 6;
+            this.labelDirectorDelete.Text = "Director:";
+            // 
+            // tbDirectorDelete
+            // 
+            this.tbDirectorDelete.Location = new System.Drawing.Point(117, 140);
+            this.tbDirectorDelete.Name = "tbDirectorDelete";
+            this.tbDirectorDelete.Size = new System.Drawing.Size(188, 20);
+            this.tbDirectorDelete.TabIndex = 7;
+            // 
+            // labelGenreDelete
+            // 
+            this.labelGenreDelete.AutoSize = true;
+            this.labelGenreDelete.Location = new System.Drawing.Point(72, 183);
+            this.labelGenreDelete.Name = "labelGenreDelete";
+            this.labelGenreDelete.Size = new System.Drawing.Size(39, 13);
+            this.labelGenreDelete.TabIndex = 8;
+            this.labelGenreDelete.Text = "Genre:";
+            // 
+            // comboBoxGenreDelete
+            // 
+            this.comboBoxGenreDelete.FormattingEnabled = true;
+            this.comboBoxGenreDelete.Location = new System.Drawing.Point(117, 180);
+            this.comboBoxGenreDelete.Name = "comboBoxGenreDelete";
+            this.comboBoxGenreDelete.Size = new System.Drawing.Size(188, 21);
+            this.comboBoxGenreDelete.TabIndex = 9;
+            // 
+            // labelRottenTomatoesScoreDelete
+            // 
+            this.labelRottenTomatoesScoreDelete.AutoSize = true;
+            this.labelRottenTomatoesScoreDelete.Location = new System.Drawing.Point(38, 240);
+            this.labelRottenTomatoesScoreDelete.Name = "labelRottenTomatoesScoreDelete";
+            this.labelRottenTomatoesScoreDelete.Size = new System.Drawing.Size(123, 13);
+            this.labelRottenTomatoesScoreDelete.TabIndex = 10;
+            this.labelRottenTomatoesScoreDelete.Text = "Rotten Tomatoes Score:";
+            // 
+            // tbRottenTomatoesScoreDelete
+            // 
+            this.tbRottenTomatoesScoreDelete.Location = new System.Drawing.Point(167, 237);
+            this.tbRottenTomatoesScoreDelete.Name = "tbRottenTomatoesScoreDelete";
+            this.tbRottenTomatoesScoreDelete.Size = new System.Drawing.Size(100, 20);
+            this.tbRottenTomatoesScoreDelete.TabIndex = 11;
+            // 
+            // labelBoxOfficeEarningsDelete
+            // 
+            this.labelBoxOfficeEarningsDelete.AutoSize = true;
+            this.labelBoxOfficeEarningsDelete.Location = new System.Drawing.Point(273, 240);
+            this.labelBoxOfficeEarningsDelete.Name = "labelBoxOfficeEarningsDelete";
+            this.labelBoxOfficeEarningsDelete.Size = new System.Drawing.Size(103, 13);
+            this.labelBoxOfficeEarningsDelete.TabIndex = 12;
+            this.labelBoxOfficeEarningsDelete.Text = "Box Office Earnings:";
+            // 
+            // tbBoxOfficeEarningsDelete
+            // 
+            this.tbBoxOfficeEarningsDelete.Location = new System.Drawing.Point(382, 237);
+            this.tbBoxOfficeEarningsDelete.Name = "tbBoxOfficeEarningsDelete";
+            this.tbBoxOfficeEarningsDelete.Size = new System.Drawing.Size(165, 20);
+            this.tbBoxOfficeEarningsDelete.TabIndex = 13;
+            // 
+            // panel1Delete
+            // 
+            this.panel1Delete.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1Delete.Controls.Add(this.btCloseDelete);
+            this.panel1Delete.Controls.Add(this.btClearDelete);
+            this.panel1Delete.Controls.Add(this.btDeleteDelete);
+            this.panel1Delete.Location = new System.Drawing.Point(12, 302);
+            this.panel1Delete.Name = "panel1Delete";
+            this.panel1Delete.Size = new System.Drawing.Size(576, 57);
+            this.panel1Delete.TabIndex = 14;
+            // 
+            // btDeleteDelete
+            // 
+            this.btDeleteDelete.Location = new System.Drawing.Point(5, 18);
+            this.btDeleteDelete.Name = "btDeleteDelete";
+            this.btDeleteDelete.Size = new System.Drawing.Size(75, 23);
+            this.btDeleteDelete.TabIndex = 0;
+            this.btDeleteDelete.Text = "Delete";
+            this.btDeleteDelete.UseVisualStyleBackColor = true;
+            // 
+            // btClearDelete
+            // 
+            this.btClearDelete.Location = new System.Drawing.Point(105, 18);
+            this.btClearDelete.Name = "btClearDelete";
+            this.btClearDelete.Size = new System.Drawing.Size(75, 23);
+            this.btClearDelete.TabIndex = 1;
+            this.btClearDelete.Text = "Clear";
+            this.btClearDelete.UseVisualStyleBackColor = true;
+            // 
+            // btCloseDelete
+            // 
+            this.btCloseDelete.Location = new System.Drawing.Point(487, 18);
+            this.btCloseDelete.Name = "btCloseDelete";
+            this.btCloseDelete.Size = new System.Drawing.Size(75, 23);
+            this.btCloseDelete.TabIndex = 2;
+            this.btCloseDelete.Text = "Close";
+            this.btCloseDelete.UseVisualStyleBackColor = true;
             // 
             // FormDeleteMovie
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.panel1Delete);
+            this.Controls.Add(this.tbBoxOfficeEarningsDelete);
+            this.Controls.Add(this.labelBoxOfficeEarningsDelete);
+            this.Controls.Add(this.tbRottenTomatoesScoreDelete);
+            this.Controls.Add(this.labelRottenTomatoesScoreDelete);
+            this.Controls.Add(this.comboBoxGenreDelete);
+            this.Controls.Add(this.labelGenreDelete);
+            this.Controls.Add(this.tbDirectorDelete);
+            this.Controls.Add(this.labelDirectorDelete);
+            this.Controls.Add(this.tbYearDelete);
+            this.Controls.Add(this.labelYearDelete);
+            this.Controls.Add(this.btFindDelete);
+            this.Controls.Add(this.tbMovieTitleDelete);
+            this.Controls.Add(this.labelMovieTitleDelete);
+            this.Controls.Add(this.labelDeleteMovieScreen);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "FormDeleteMovie";
             this.Text = "Delete Movie";
+            this.panel1Delete.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labelDeleteMovieScreen;
+        private System.Windows.Forms.Label labelMovieTitleDelete;
+        private System.Windows.Forms.TextBox tbMovieTitleDelete;
+        private System.Windows.Forms.Button btFindDelete;
+        private System.Windows.Forms.Label labelYearDelete;
+        private System.Windows.Forms.TextBox tbYearDelete;
+        private System.Windows.Forms.Label labelDirectorDelete;
+        private System.Windows.Forms.TextBox tbDirectorDelete;
+        private System.Windows.Forms.Label labelGenreDelete;
+        private System.Windows.Forms.ComboBox comboBoxGenreDelete;
+        private System.Windows.Forms.Label labelRottenTomatoesScoreDelete;
+        private System.Windows.Forms.TextBox tbRottenTomatoesScoreDelete;
+        private System.Windows.Forms.Label labelBoxOfficeEarningsDelete;
+        private System.Windows.Forms.TextBox tbBoxOfficeEarningsDelete;
+        private System.Windows.Forms.Panel panel1Delete;
+        private System.Windows.Forms.Button btDeleteDelete;
+        private System.Windows.Forms.Button btCloseDelete;
+        private System.Windows.Forms.Button btClearDelete;
     }
 }
--- MovieManager/UpdateMovie.Designer.cs
+++ MovieManager/UpdateMovie.Designer.cs
@@ -29,20 +29,230 @@ namespace MovieManager
         /// </summary>
         private void InitializeComponent()
         {
+            this.labelUpdateMovieScreen = new System.Windows.Forms.Label();
+            this.labelMovieTitleUpdate = new System.Windows.Forms.Label();
+            this.tbMovieTitleUpdate = new System.Windows.Forms.TextBox();
+            this.btFindUpdate = new System.Windows.Forms.Button();
+            this.labelYearUpdate = new System.Windows.Forms.Label();
+            this.tbYearUpdate = new System.Windows.Forms.TextBox();
+            this.labelDirectorUpdate = new System.Windows.Forms.Label();
+            this.tbDirectorUpdate = new System.Windows.Forms.TextBox();
+            this.labelGenreUpdate = new System.Windows.Forms.Label();
+            this.comboBoxGenreUpdate = new System.Windows.Forms.ComboBox();
+            this.labelRottenTomatoesScoreUpdate = new System.Windows.Forms.Label();
+            this.tbRottenTomatoesScoreUpdate = new System.Windows.Forms.TextBox();
+            this.labelBoxOfficeEarningsUpdate = new System.Windows.Forms.Label();
+            this.tbBoxOfficeEarningsUpdate = new System.Windows.Forms.TextBox();
+            this.panel1Update = new System.Windows.Forms.Panel();
+            this.btUpdateUpdate = new System.Windows.Forms.Button();
+            this.btClearUpdate = new System.Windows.Forms.Button();
+            this.btCloseUpdate = new System.Windows.Forms.Button();
+            this.panel1Update.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // labelUpdateMovieScreen
+            // 
+            this.labelUpdateMovieScreen.AutoSize = true;
+            this.labelUpdateMovieScreen.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelUpdateMovieScreen.Location = new System.Drawing.Point(12, 9);
+            this.labelUpdateMovieScreen.Name = "labelUpdateMovieScreen";
+            this.labelUpdateMovieScreen.Size = new System.Drawing.Size(264, 29);
+            this.labelUpdateMovieScreen.TabIndex = 0;
+            this.labelUpdateMovieScreen.Text = "Update Movie Screen";
+            // 
+            // labelMovieTitleUpdate
+            // 
+            this.labelMovieTitleUpdate.AutoSize = true;
+            this.labelMovieTitleUpdate.Location = new System.Drawing.Point(52, 63);
+            this.labelMovieTitleUpdate.Name = "labelMovieTitleUpdate";
+            this.labelMovieTitleUpdate.Size = new System.Drawing.Size(62, 13);
+            this.labelMovieTitleUpdate.TabIndex = 1;
+            this.labelMovieTitleUpdate.Text = "Movie Title:";
+            // 
+            // tbMovieTitleUpdate
+            // 
+            this.tbMovieTitleUpdate.Location = new System.Drawing.Point(117, 60);
+            this.tbMovieTitleUpdate.Name = "tbMovieTitleUpdate";
+            this.tbMovieTitleUpdate.Size = new System.Drawing.Size(297, 20);
+            this.tbMovieTitleUpdate.TabIndex = 2;
+            // 
+            // btFindUpdate
+            // 
+            this.btFindUpdate.Location = new System.Drawing.Point(420, 58);
+            this.btFindUpdate.Name = "btFindUpdate";
+            this.btFindUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btFindUpdate.TabIndex = 3;
+            this.btFindUpdate.Text = "Find";
+            this.btFindUpdate.UseVisualStyleBackColor = true;
+            // 
+            // labelYearUpdate
+            // 
+            this.labelYearUpdate.AutoSize = true;
+            this.labelYearUpdate.Location = new System.Drawing.Point(79, 103);
+            this.labelYearUpdate.Name = "labelYearUpdate";
+            this.labelYearUpdate.Size = new System.Drawing.Size(32, 13);
+            this.labelYearUpdate.TabIndex = 4;
+            this.labelYearUpdate.Text = "Year:";
+            // 
+            // tbYearUpdate
+            // 
+            this.tbYearUpdate.Location = new System.Drawing.Point(117, 100);
+            this.tbYearUpdate.Name = "tbYearUpdate";
+            this.tbYearUpdate.Size = new System.Drawing.Size(116, 20);
+            this.tbYearUpdate.TabIndex = 5;
+            // 
+            // labelDirectorUpdate
+            // 
+            this.labelDirectorUpdate.AutoSize = true;
+            this.labelDirectorUpdate.Location = new System.Drawing.Point(64, 143);
+            this.labelDirectorUpdate.Name = "labelDirectorUpdate";
+            this.labelDirectorUpdate.Size = new System.Drawing.Size(47, 13);
+            this.labelDirectorUpdate.TabIndex = 6;
+            this.labelDirectorUpdate.Text = "Director:";
+            // 
+            // tbDirectorUpdate
+            // 
+            this.tbDirectorUpdate.Location = new System.Drawing.Point(117, 140);
+            this.tbDirectorUpdate.Name = "tbDirectorUpdate";
+            this.tbDirectorUpdate.Size = new System.Drawing.Size(188, 20);
+            this.tbDirectorUpdate.TabIndex = 7;
+            // 
+            // labelGenreUpdate
+            // 
+            this.labelGenreUpdate.AutoSize = true;
+            this.labelGenreUpdate.Location = new System.Drawing.Point(72, 183);
+            this.labelGenreUpdate.Name = "labelGenreUpdate";
+            this.labelGenreUpdate.Size = new System.Drawing.Size(39, 13);
+            this.labelGenreUpdate.TabIndex = 8;
+            this.labelGenreUpdate.Text = "Genre:";
+            // 
+            // comboBoxGenreUpdate
+            // 
+            this.comboBoxGenreUpdate.FormattingEnabled = true;
+            this.comboBoxGenreUpdate.Location = new System.Drawing.Point(117, 180);
+            this.comboBoxGenreUpdate.Name = "comboBoxGenreUpdate";
+            this.comboBoxGenreUpdate.Size = new System.Drawing.Size(188, 21);
+            this.comboBoxGenreUpdate.TabIndex = 9;
+            // 
+            // labelRottenTomatoesScoreUpdate
+            // 
+            this.labelRottenTomatoesScoreUpdate.AutoSize = true;
+            this.labelRottenTomatoesScoreUpdate.Location = new System.Drawing.Point(38, 240);
+            this.labelRottenTomatoesScoreUpdate.Name = "labelRottenTomatoesScoreUpdate";
+            this.labelRottenTomatoesScoreUpdate.Size = new System.Drawing.Size(123, 13);
+            this.labelRottenTomatoesScoreUpdate.TabIndex = 10;
+            this.labelRottenTomatoesScoreUpdate.Text = "Rotten Tomatoes Score:";
+            // 
+            // tbRottenTomatoesScoreUpdate
+            // 
+            this.tbRottenTomatoesScoreUpdate.Location = new System.Drawing.Point(167, 237);
+            this.tbRottenTomatoesScoreUpdate.Name = "tbRottenTomatoesScoreUpdate";
+            this.tbRottenTomatoesScoreUpdate.Size = new System.Drawing.Size(100, 20);
+            this.tbRottenTomatoesScoreUpdate.TabIndex = 11;
+            // 
+            // labelBoxOfficeEarningsUpdate
+            // 
+            this.labelBoxOfficeEarningsUpdate.AutoSize = true;
+            this.labelBoxOfficeEarningsUpdate.Location = new System.Drawing.Point(273, 240);
+            this.labelBoxOfficeEarningsUpdate.Name = "labelBoxOfficeEarningsUpdate";
+            this.labelBoxOfficeEarningsUpdate.Size = new System.Drawing.Size(103, 13);
+            this.labelBoxOfficeEarningsUpdate.TabIndex = 12;
+            this.labelBoxOfficeEarningsUpdate.Text = "Box Office Earnings:";
+            // 
+            // tbBoxOfficeEarningsUpdate
+            // 
+            this.tbBoxOfficeEarningsUpdate.Location = new System.Drawing.Point(382, 237);
+            this.tbBoxOfficeEarningsUpdate.Name = "tbBoxOfficeEarningsUpdate";
+            this.tbBoxOfficeEarningsUpdate.Size = new System.Drawing.Size(165, 20);
+            this.tbBoxOfficeEarningsUpdate.TabIndex = 13;
+            // 
+            // panel1Update
+            // 
+            this.panel1Update.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1Update.Controls.Add(this.btCloseUpdate);
+            this.panel1Update.Controls.Add(this.btClearUpdate);
+            this.panel1Update.Controls.Add(this.btUpdateUpdate);
+            this.panel1Update.Location = new System.Drawing.Point(12, 302);
+            this.panel1Update.Name = "panel1Update";
+            this.panel1Update.Size = new System.Drawing.Size(576, 57);
+            this.panel1Update.TabIndex = 14;
+            // 
+            // btUpdateUpdate
+            // 
+            this.btUpdateUpdate.Location = new System.Drawing.Point(5, 18);
+            this.btUpdateUpdate.Name = "btUpdateUpdate";
+            this.btUpdateUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btUpdateUpdate.TabIndex = 0;
+            this.btUpdateUpdate.Text = "Update";
+            this.btUpdateUpdate.UseVisualStyleBackColor = true;
+            // 
+            // btClearUpdate
+            // 
+            this.btClearUpdate.Location = new System.Drawing.Point(105, 18);
+            this.btClearUpdate.Name = "btClearUpdate";
+            this.btClearUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btClearUpdate.TabIndex = 1;
+            this.btClearUpdate.Text = "Clear";
+            this.btClearUpdate.UseVisualStyleBackColor = true;
+            // 
+            // btCloseUpdate
+            // 
+            this.btCloseUpdate.Location = new System.Drawing.Point(487, 18);
+            this.btCloseUpdate.Name = "btCloseUpdate";
+            this.btCloseUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btCloseUpdate.TabIndex = 2;
+            this.btCloseUpdate.Text = "Close";
+            this.btCloseUpdate.UseVisualStyleBackColor = true;
             // 
             // FormUpdateMovie
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.panel1Update);
+            this.Controls.Add(this.tbBoxOfficeEarningsUpdate);
+            this.Controls.Add(this.labelBoxOfficeEarningsUpdate);
+            this.Controls.Add(this.tbRottenTomatoesScoreUpdate);
+            this.Controls.Add(this.labelRottenTomatoesScoreUpdate);
+            this.Controls.Add(this.comboBoxGenreUpdate);
+            this.Controls.Add(this.labelGenreUpdate);
+            this.Controls.Add(this.tbDirectorUpdate);
+            this.Controls.Add(this.labelDirectorUpdate);
+            this.Controls.Add(this.tbYearUpdate);
+            this.Controls.Add(this.labelYearUpdate);
+            this.Controls.Add(this.btFindUpdate);
+            this.Controls.Add(this.tbMovieTitleUpdate);
+            this.Controls.Add(this.labelMovieTitleUpdate);
+            this.Controls.Add(this.labelUpdateMovieScreen);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "FormUpdateMovie";
             this.Text = "Update Movie";
             this.Load += new System.EventHandler(this.UpdateMovie_Load);
+            this.panel1Update.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labelUpdateMovieScreen;
+        private System.Windows.Forms.Label labelMovieTitleUpdate;
+        private System.Windows.Forms.TextBox tbMovieTitleUpdate;
+        private System.Windows.Forms.Button btFindUpdate;
+        private System.Windows.Forms.Label labelYearUpdate;
+        private System.Windows.Forms.TextBox tbYearUpdate;
+        private System.Windows.Forms.Label labelDirectorUpdate;
+        private System.Windows.Forms.TextBox tbDirectorUpdate;
+        private System.Windows.Forms.Label labelGenreUpdate;
+        private System.Windows.Forms.ComboBox comboBoxGenreUpdate;
+        private System.Windows.Forms.Label labelRottenTomatoesScoreUpdate;
+        private System.Windows.Forms.TextBox tbRottenTomatoesScoreUpdate;
+        private System.Windows.Forms.Label labelBoxOfficeEarningsUpdate;
+        private System.Windows.Forms.TextBox tbBoxOfficeEarningsUpdate;
+        private System.Windows.Forms.Panel panel1Update;
+        private System.Windows.Forms.Button btCloseUpdate;
+        private System.Windows.Forms.Button btClearUpdate;
+        private System.Windows.Forms.Button btUpdateUpdate;
     }
 }
