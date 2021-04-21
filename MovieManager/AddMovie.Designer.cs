727ad8338560e5ac074e43262327caf85a69a61e
George Raedeke <graedeke@student.cscc.edu>
2021-04-21 16:08:04 -0400

    Buttons on Add

--- MovieManager/AddMovie.Designer.cs
+++ MovieManager/AddMovie.Designer.cs
@@ -42,6 +42,11 @@ namespace MovieManager
             this.textBox4 = new System.Windows.Forms.TextBox();
             this.textBox5 = new System.Windows.Forms.TextBox();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btAdd = new System.Windows.Forms.Button();
+            this.btClearAdd = new System.Windows.Forms.Button();
+            this.btCloseAdd = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -49,98 +54,110 @@ namespace MovieManager
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Times New Roman", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(178, 38);
+            this.label1.Size = new System.Drawing.Size(143, 31);
             this.label1.TabIndex = 0;
             this.label1.Text = "Add Movie";
             // 
             // labelMovieTitle
             // 
             this.labelMovieTitle.AutoSize = true;
-            this.labelMovieTitle.Location = new System.Drawing.Point(87, 68);
+            this.labelMovieTitle.Location = new System.Drawing.Point(65, 58);
+            this.labelMovieTitle.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelMovieTitle.Name = "labelMovieTitle";
-            this.labelMovieTitle.Size = new System.Drawing.Size(76, 17);
+            this.labelMovieTitle.Size = new System.Drawing.Size(59, 13);
             this.labelMovieTitle.TabIndex = 1;
             this.labelMovieTitle.Text = "Movie Title";
             // 
             // labelYear
             // 
             this.labelYear.AutoSize = true;
-            this.labelYear.Location = new System.Drawing.Point(87, 127);
+            this.labelYear.Location = new System.Drawing.Point(95, 108);
+            this.labelYear.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelYear.Name = "labelYear";
-            this.labelYear.Size = new System.Drawing.Size(38, 17);
+            this.labelYear.Size = new System.Drawing.Size(29, 13);
             this.labelYear.TabIndex = 2;
             this.labelYear.Text = "Year";
             // 
             // labelDirector
             // 
             this.labelDirector.AutoSize = true;
-            this.labelDirector.Location = new System.Drawing.Point(87, 220);
+            this.labelDirector.Location = new System.Drawing.Point(80, 158);
+            this.labelDirector.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelDirector.Name = "labelDirector";
-            this.labelDirector.Size = new System.Drawing.Size(58, 17);
+            this.labelDirector.Size = new System.Drawing.Size(44, 13);
             this.labelDirector.TabIndex = 3;
             this.labelDirector.Text = "Director";
             // 
             // labelGenre
             // 
             this.labelGenre.AutoSize = true;
-            this.labelGenre.Location = new System.Drawing.Point(87, 304);
+            this.labelGenre.Location = new System.Drawing.Point(88, 208);
+            this.labelGenre.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelGenre.Name = "labelGenre";
-            this.labelGenre.Size = new System.Drawing.Size(48, 17);
+            this.labelGenre.Size = new System.Drawing.Size(36, 13);
             this.labelGenre.TabIndex = 4;
             this.labelGenre.Text = "Genre";
             // 
             // labelTomatoes
             // 
             this.labelTomatoes.AutoSize = true;
-            this.labelTomatoes.Location = new System.Drawing.Point(87, 372);
+            this.labelTomatoes.Location = new System.Drawing.Point(65, 254);
+            this.labelTomatoes.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelTomatoes.Name = "labelTomatoes";
-            this.labelTomatoes.Size = new System.Drawing.Size(158, 17);
+            this.labelTomatoes.Size = new System.Drawing.Size(120, 13);
             this.labelTomatoes.TabIndex = 5;
             this.labelTomatoes.Text = "Rotten Tomatoes Score";
             // 
             // labelBoxOffice
             // 
             this.labelBoxOffice.AutoSize = true;
-            this.labelBoxOffice.Location = new System.Drawing.Point(412, 372);
+            this.labelBoxOffice.Location = new System.Drawing.Point(300, 254);
+            this.labelBoxOffice.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelBoxOffice.Name = "labelBoxOffice";
-            this.labelBoxOffice.Size = new System.Drawing.Size(132, 17);
+            this.labelBoxOffice.Size = new System.Drawing.Size(100, 13);
             this.labelBoxOffice.TabIndex = 6;
             this.labelBoxOffice.Text = "Box Office Earnings";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(194, 68);
+            this.textBox1.Location = new System.Drawing.Point(146, 55);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(301, 22);
+            this.textBox1.Size = new System.Drawing.Size(254, 20);
             this.textBox1.TabIndex = 7;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(194, 127);
+            this.textBox2.Location = new System.Drawing.Point(146, 105);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.Size = new System.Drawing.Size(76, 20);
             this.textBox2.TabIndex = 8;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(194, 220);
+            this.textBox3.Location = new System.Drawing.Point(146, 155);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.Size = new System.Drawing.Size(150, 20);
             this.textBox3.TabIndex = 9;
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(276, 369);
+            this.textBox4.Location = new System.Drawing.Point(189, 251);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(100, 22);
+            this.textBox4.Size = new System.Drawing.Size(107, 20);
             this.textBox4.TabIndex = 10;
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(567, 367);
+            this.textBox5.Location = new System.Drawing.Point(404, 251);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(100, 22);
+            this.textBox5.Size = new System.Drawing.Size(127, 20);
             this.textBox5.TabIndex = 11;
             // 
             // comboBox1
@@ -156,16 +173,57 @@ namespace MovieManager
             "Romance",
             "Science Fiction",
             "Western"});
-            this.comboBox1.Location = new System.Drawing.Point(194, 301);
+            this.comboBox1.Location = new System.Drawing.Point(146, 205);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.Size = new System.Drawing.Size(150, 21);
             this.comboBox1.TabIndex = 12;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.btCloseAdd);
+            this.panel1.Controls.Add(this.btClearAdd);
+            this.panel1.Controls.Add(this.btAdd);
+            this.panel1.Location = new System.Drawing.Point(12, 302);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(576, 57);
+            this.panel1.TabIndex = 13;
+            // 
+            // btAdd
+            // 
+            this.btAdd.Location = new System.Drawing.Point(5, 18);
+            this.btAdd.Name = "btAdd";
+            this.btAdd.Size = new System.Drawing.Size(75, 23);
+            this.btAdd.TabIndex = 0;
+            this.btAdd.Text = "Add";
+            this.btAdd.UseVisualStyleBackColor = true;
+            // 
+            // btClearAdd
+            // 
+            this.btClearAdd.Location = new System.Drawing.Point(105, 18);
+            this.btClearAdd.Name = "btClearAdd";
+            this.btClearAdd.Size = new System.Drawing.Size(75, 23);
+            this.btClearAdd.TabIndex = 1;
+            this.btClearAdd.Text = "Clear";
+            this.btClearAdd.UseVisualStyleBackColor = true;
+            // 
+            // btCloseAdd
+            // 
+            this.btCloseAdd.Location = new System.Drawing.Point(487, 18);
+            this.btCloseAdd.Name = "btCloseAdd";
+            this.btCloseAdd.Size = new System.Drawing.Size(75, 23);
+            this.btCloseAdd.TabIndex = 2;
+            this.btCloseAdd.Text = "Close";
+            this.btCloseAdd.UseVisualStyleBackColor = true;
+            this.btCloseAdd.Click += new System.EventHandler(this.btCloseAdd_Click);
             // 
             // FormAddMovie
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.textBox5);
             this.Controls.Add(this.textBox4);
@@ -179,8 +237,10 @@ namespace MovieManager
             this.Controls.Add(this.labelYear);
             this.Controls.Add(this.labelMovieTitle);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "FormAddMovie";
             this.Text = "Add Movie";
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -201,5 +261,9 @@ namespace MovieManager
         private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.TextBox textBox5;
         private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btCloseAdd;
+        private System.Windows.Forms.Button btClearAdd;
+        private System.Windows.Forms.Button btAdd;
     }
 }
--- MovieManager/AddMovie.cs
+++ MovieManager/AddMovie.cs
@@ -16,5 +16,10 @@ namespace MovieManager
         {
             InitializeComponent();
         }
+
+        private void btCloseAdd_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
     }
 }
